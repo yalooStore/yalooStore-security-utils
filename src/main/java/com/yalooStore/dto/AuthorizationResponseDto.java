8595df7e2cb4b9db055dc742c0f0a6ddfca69ca7
hwiju yeom <yeom456@gmail.com>
2023-07-10 18:40:14 +0900

    Update AuthorizationResponseDto.java

--- src/main/java/com/yalooStore/dto/AuthorizationResponseDto.java
+++ src/main/java/com/yalooStore/dto/AuthorizationResponseDto.java
@@ -1,10 +1,14 @@
 package com.yalooStore.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorizationResponseDto {
     private String loginId;
     private List<String> authority;
